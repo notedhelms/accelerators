a13f25b256a98c4c3e3f01ddd3e0c4745d570229
Seth Meldon <seth.meldon@progress.com>
2023-09-29 10:17:20 -0400

    update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Progress Corticon', // Usually your GitHub org/user name.
-  projectName: 'accelerate', // Usually your repo name.
+  projectName: 'accelerators', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
