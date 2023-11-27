06c6918fe2088389010c7b0f9279afdd4d58fd14
Seth Meldon <seth.meldon@progress.com>
2023-11-27 10:30:10 -0500

    delete 1 file and update 1 file

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/README">
+            to="/docs/intro">
             Browse Templates and Tutorials
           </Link>
         </div>
